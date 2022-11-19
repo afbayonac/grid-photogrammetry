f4bc824b5e709a0b6d86d910dbf6557db88a1122
aratoca <afbayonac@gmail.com>
2022-11-19 16:00:15 -0500

    Recalcule route

---
+++ README.md
@@ -0,0 +1,5 @@
+# Grid Photogrammetry
+Referenced: https://github.com/smt5965/mission-planner-/blob/ddaf805a204aa4f89d716ebc3d10b97bbef92bee/ExtLibs/Utilities/Grid.cs#L315
+
+## author
+@afbayona
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const { createApp } = Vue
+const emitter = mitt()
+const DEBUG = false;
 
 Math.grados = function(radianes) {
   return radianes * 180 / Math.PI;
@@ -8,70 +10,52 @@ Math.radianes = function(grados) {
   return Math.PI * grados / 180;
 };
 
-const DEBUG = true;
-// const event = new Event('onCameraParameters');
-const featureId = null;
+const initMap = () => {
+  const draw = new MapboxDraw({
+    controls: {
+      combine_features: false,
+      point: false,
+      line_string: false,
+      uncombine_features: false
+    },
+    mode: 'draw_polygon'
+  });
+  
+  const style = {
+    "version": 8,
+    "sources": {
+      "osm": {
+        "type": "raster",
+        "tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
+        "tileSize": 256,
+        "attribution": "&copy; OpenStreetMap Contributors",
+        "maxzoom": 19
+      }
+    },
+    "layers": [
+      {
+        "id": "osm",
+        "type": "raster",
+        "source": "osm" // This must match the source key above
+      }
+    ]
+  };
+  
+  const map = new maplibregl.Map({
+    container: 'map',
+    style, 
+    center: [-74.107807, 4.6482975], // [lng, lat]
+    zoom: 17
+  });
+  
+  map.addControl(draw, 'top-right');
 
-// create map view 
-const draw = new MapboxDraw({
-  controls: {
-    combine_features: false,
-    point: false,
-    line_string: false,
-    uncombine_features: false
-  }
-});
-
-const style = {
-  "version": 8,
-	"sources": {
-    "osm": {
-			"type": "raster",
-			"tiles": ["https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"],
-			"tileSize": 256,
-      "attribution": "&copy; OpenStreetMap Contributors",
-      "maxzoom": 19
-    }
-  },
-  "layers": [
-    {
-      "id": "osm",
-      "type": "raster",
-      "source": "osm" // This must match the source key above
-    }
-  ]
-};
-
-const map = new maplibregl.Map({
-  container: 'map',
-  style, // stylesheet location
-  center: [-74.107807, 4.6482975], // starting position [lng, lat]
-  zoom: 17
-});
-
-map.addControl(draw, 'top-right');
-
-// elem.addEventListener('build', (e) => { /* … */ }, false);
-
-const PolygonView = {
-  data() {
-    return {
-      polygon: {}
-    }
-  },
-  methods: {
-    setPolygon(polygon) {
-      console.log('set polygon')
-      this.polygon = JSON.stringify(polygon, null, 2)
-    }
-  },
-
-  template: `<pre><code>{{ polygon }}</code></pre>`
+  return [map, draw]
 }
 
-const polygonView = createApp(PolygonView).mount('#polygon-view')
+const [map, draw] = initMap()
 
-const CameraParams = {
+const Control = {
   data() {
     return {
       focalLength: 8.4,
@@ -79,110 +63,194 @@ const CameraParams = {
       imageHeight: 3648,
       sensorWidth: 13.31,
       sensorHeight: 8.88,
-      verticalDistance: 50,
+      flyHeight: 50,
       overlap: 80,
       sidelap: 80,
       angle: 0,
-      showFrames: true
+      showFrames: false,
+      showCameras: true,
+      images: 0,
+      area: 0,
+      route: null
     }
   },
   methods: {
+    setArea(area) {
+      this.area = area
+    },
+    setImages(images) {
+      this.images = images
+    },
+    setRoute(route) {
+      this.route = route
+    }
   },
   computed: {
     GDSW () {
-      return  ((this.sensorWidth / 10) / this.imageWidth ) * (this.verticalDistance * 100) / (this.focalLength / 10) 
+      return  ((this.sensorWidth / 10) / this.imageWidth ) * (this.flyHeight * 100) / (this.focalLength / 10) 
     }, 
     GDSH () {
-      return   ((this.sensorHeight / 10) / this.imageHeight )  * (this.verticalDistance * 100) / (this.focalLength / 10)
+      return   ((this.sensorHeight / 10) / this.imageHeight )  * (this.flyHeight * 100) / (this.focalLength / 10)
     },
     CoverturaW () {
-      return  (this.sensorWidth / 10) * (this.verticalDistance * 100) / (this.focalLength / 10) 
+      return  (this.sensorWidth / 10) * (this.flyHeight * 100) / (this.focalLength / 10) 
     }, 
     CoverturaH () {
-      return   (this.sensorHeight / 10)  * (this.verticalDistance * 100) / (this.focalLength / 10)
+      return   (this.sensorHeight / 10)  * (this.flyHeight * 100) / (this.focalLength / 10)
     }
   },
   watch: {
-    '$data': {
-      handler: function (value) {
-        console.log(value)
-      },
-      deep: true
+    focalLength () {
+      emitter.emit("control.update")
+    },
+    imageWidth () {
+      emitter.emit("control.update")
+    },
+    imageHeight () {
+      emitter.emit("control.update")
+    },
+    sensorWidth () {
+      emitter.emit("control.update")
+    },
+    sensorHeight () {
+      emitter.emit("control.update")
+    },
+    flyHeight () {
+      emitter.emit("control.update")
+    },
+    overlap () {
+      emitter.emit("control.update")
+    },
+    sidelap () {
+      emitter.emit("control.update")
+    },
+    angle () {
+      emitter.emit("control.update")
+    },
+    showFrames () {
+      emitter.emit("control.update")
+    },
+    showCameras () {
+      emitter.emit("control.update")
     }
   },
   template: `
-    <input type="number" v-model="focalLength" max="100" min="0" length="3" step="0.01"/> [mm]
-    <input type="number" v-model="imageWidth" max="10000" min="0" /> [px]
-    <input type="number" v-model="imageHeight" max="10000" min="0" /> [px]
-    <input type="number" v-model="sensorWidth" max="100" min="0" step="0.01"/> [mm]
-    <input type="number" v-model="sensorHeight"  max="100" min="0" step="0.01"/> [mm]
-    <input type="number" v-model="verticalDistance"  max="500" min="0" step="10"/> [m]  
-    <input type="number" v-model="angle"  max="359" min="0" step="1"/> [º]   
-    <input type="number" v-model="overlap"  max="99" min="0" step="1"/> [%]  
-    <input type="number" v-model="sidelap"  max="99" min="0" step="1"/> [%]
-    <input type="checkbox" v-model="showFrames" />
-    <div class="font-mono">
+    <h2 class="text-center mb-2">Params</h2>
+    <div class="grid grid-cols-2 w-80 mx-2 mb-5 font-mono">
+      <label>Focal Length : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="focalLength" max="100" min="0"  step="0.01"/> [mm]
+      </span>
+      
+      <label>Image Width : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="imageWidth" max="100000" min="0"  step="1"/> [px]
+      </span>
+
+      <label>Image Height : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="imageHeight" max="100000" min="0"  step="1"/> [px]
+      </span>
+
+      <label>Sensor Width : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="sensorWidth" max="100" min="0"  step="0.01"/> [mm]
+      </span>
+
+      <label>Sensor Height : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="sensorHeight" max="100" min="0"  step="0.01"/> [mm]
+      </span>
+
+      <label>Fly Height : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="flyHeight" max="1000" min="0"  step="10"/> [m]
+      </span>
+
+      <label>Angle : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="angle" max="360" min="0"  step="1"/> [º]
+      </span>
+
+      <label>Overlap : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="overlap" max="99" min="0"  step="1"/> [%]
+      </span>
+
+      <label>Sidelap : </label>
+      <span class="align-middle">
+        <input class="border block-inline text-right w-24" type="number" v-model="sidelap" max="99" min="0"  step="1"/> [%]
+      </span>
+  
+      <label>Show Frames : </label>
+      <span class="align-middle">
+        <input type="checkbox" v-model="showFrames" />
+      </span>
+
+
+      <label>Show Cameras : </label>
+      <span class="align-middle">
+        <input type="checkbox" v-model="showCameras" />
+      </span>
+    </div>
+    
+    <div class="font-mono mb-3 mx-2">
       <div>GDS<sub>w</sub> = {{Math.round(GDSW * 100) / 100}} cm </div> 
       <div>GDS<sub>h</sub> = {{Math.round(GDSH * 100) / 100}} cm </div> 
       <div>Covertura<sub>w</sub> = {{Math.round(CoverturaW) / 100}} m </div> 
       <div>Covertura<sub>h</sub> = {{Math.round(CoverturaH) / 100}} m </div> 
+      <div>Area = {{area}} m² </div> 
+      <div>Images = {{images}}</div> 
+    </div>
+
+    <div class="text-center mb-3">
+      <button class="px-4 py-2 font-semibold text-sm bg-sky-500 text-white rounded-none shadow-sm"> Download Lichit CVS</button>
     </div>
   `
 }
 
-const params = createApp(CameraParams).mount('#params')
-
-map.on('draw.create', e => {
-  const polygon = e.features[0]
-  console.log(`--- ${polygon.id} ---`)
-  draw
-    .getAll()
-    .features
-    .map(f =>
-      f.id !== polygon.id &&
-      draw.delete(f.id)
-    )
-
-  const { CoverturaW, CoverturaH, angle, sidelap, overlap } = params
-  // const CoverturaHKm = CoverturaH / 100000
-  // const CoverturaWKm = CoverturaW / 100000
+const control = createApp(Control).mount('#control')
+console.log(control)
+const updateRoute = (draw, polygon, control) => {
+  if (!polygon) return
   
-  // if (DEBUG) {
-  //   console.log('CoverturaHKm', CoverturaHKm)
-  //   console.log('CoverturaWKm', CoverturaWKm)
-  // }
-
+  control.setArea(turf.area(polygon))
+  const { CoverturaW, CoverturaH, angle, sidelap, overlap, showCameras, showFrames } = control
   const stepW = CoverturaW / 100000 * (1 - (sidelap / 100))
   const stepH = CoverturaH / 100000 * (1 - (overlap / 100))
 
-  const route = getRoute(angle, stepW, polygon)  
+  const route = genRoute(angle, stepW, polygon)  
 
+  control.setRoute(route)
 
-  route.map(s => {
+  const points = route
+  .reduce((points, s) => {
     const distance = turf.distance(s[0], s[1])
     const angle = turf.bearing(s[0], s[1])
 
 
-    Array(Math.ceil( distance / stepH))
-      .fill(null)
-      .map((_, i) => turf.rhumbDestination(s[0], stepH * i, angle))
-      .map((p)=> {
-        draw.add(frameFeature(CoverturaW, CoverturaH, angle, p, '1'))
-        draw.add(p)
-      })
+    return [
+      ...points, 
+      ...Array(Math.ceil( distance / stepH))
+        .fill(null)
+        .map((_, i) => turf.rhumbDestination(s[0], stepH * i, angle))
+    ]
+  }, [])
 
+
+  control.setImages(points.length)
+
+  points
+  .map(p => {
+    showFrames && draw.add(frame(CoverturaW, CoverturaH, angle, p))
+    showCameras && draw.add(p)
   })
 
-  console.log(route)
   draw.add(
     {
-      // id: 'debug-1',
+      id: 'gf-route',
       type: 'Feature',
-      // properties: {},
-      properties: {
-        name: 'Aconcagua',
-        height: 200
-      },
+      properties: {},
       geometry: { 
         type: 'LineString', 
         coordinates: route
@@ -191,11 +259,9 @@ map.on('draw.create', e => {
       }
     }
   )
-  polygonView.setPolygon(polygon)
-})
+} 
 
-
-const getRoute = (angle, step, polygon) => {
+const genRoute = (angle, step, polygon) => {
   // Boundary box
   const bbox = turf.bbox(polygon);
   const pointA = turf.point([bbox[0], bbox[1]])
@@ -208,55 +274,55 @@ const getRoute = (angle, step, polygon) => {
   const hypot = turf.distance(pointA, pointB)
 
   if (DEBUG) {
-    // console.log('angle', angle)
-    // console.log('α', alfa)
-    // console.log('β', beta)
-    // console.log('h', hypot)
-    // console.log('C------B')
-    // console.log('|++++/+|')
-    // console.log('|+++/++|')
-    // console.log('|α /β++|')
-    // console.log('A______D')
+    console.log('angle', angle)
+    console.log('α', alfa)
+    console.log('β', beta)
+    console.log('h', hypot)
+    console.log('C------B')
+    console.log('|++++/+|')
+    console.log('|+++/++|')
+    console.log('|α /β++|')
+    console.log('A______D')
 
-    // draw.add({
-    //   id: 'debug-1',
-    //   type: 'Feature',
-    //   properties: {},
-    //   geometry: { type: 'LineString', coordinates: [
-    //     [bbox[0], bbox[1]],
-    //     [bbox[0], bbox[3]]
-    //   ]}
-    // })
+    draw.add({
+      id: 'gf-debug-1',
+      type: 'Feature',
+      properties: {},
+      geometry: { type: 'LineString', coordinates: [
+        [bbox[0], bbox[1]],
+        [bbox[0], bbox[3]]
+      ]}
+    })
   
-    // draw.add({
-    //   id: 'debug-2',
-    //   type: 'Feature',
-    //   properties: {},
-    //   geometry: { type: 'LineString', coordinates: [
-    //     [bbox[0], bbox[3]],
-    //     [bbox[2], bbox[3]]
-    //   ]}
-    // })
+    draw.add({
+      id: 'gf-debug-2',
+      type: 'Feature',
+      properties: {},
+      geometry: { type: 'LineString', coordinates: [
+        [bbox[0], bbox[3]],
+        [bbox[2], bbox[3]]
+      ]}
+    })
   
-    // draw.add({
-    //   id: 'debug-3',
-    //   type: 'Feature',
-    //   properties: {},
-    //   geometry: { type: 'LineString', coordinates: [
-    //     [bbox[0], bbox[3]],
-    //     [bbox[2], bbox[1]]
-    //   ]}
-    // })
+    draw.add({
+      id: 'gf-debug-3',
+      type: 'Feature',
+      properties: {},
+      geometry: { type: 'LineString', coordinates: [
+        [bbox[0], bbox[3]],
+        [bbox[2], bbox[1]]
+      ]}
+    })
   
-    // draw.add({
-    //   id: 'debug-4',
-    //   type: 'Feature',
-    //   properties: {},
-    //   geometry: { type: 'LineString', coordinates: [
-    //     [bbox[2], bbox[3]],
-    //     [bbox[0], bbox[1]]
-    //   ]}
-    // })
+    draw.add({
+      id: 'gf-debug-4',
+      type: 'Feature',
+      properties: {},
+      geometry: { type: 'LineString', coordinates: [
+        [bbox[2], bbox[3]],
+        [bbox[0], bbox[1]]
+      ]}
+    })
   }
 
   const angleIdentity = angle % 180
@@ -272,7 +338,7 @@ const getRoute = (angle, step, polygon) => {
     console.log(step, stepCorrection)
   }
  
-  const route = Array(segments + 1).fill({})
+  const route = Array(segments + 1).fill(null)
     .map((_, i) => {
       const p0 = turf.rhumbDestination(origin, stepCorrection * i, angleH)
       const p1 = turf.rhumbDestination(p0, hypot, angleIdentity).geometry.coordinates
@@ -288,7 +354,6 @@ const getRoute = (angle, step, polygon) => {
       )
     .filter(intersects => intersects.length > 0 && intersects.length % 2 === 0)
     .reduce((route, intersects) => {
-      console.log('intersects', intersects)
       return [
         ...route, 
         ...Array(intersects.length / 2)
@@ -301,7 +366,6 @@ const getRoute = (angle, step, polygon) => {
       ]
     }, [])
 
-    console.log(route)
     sortRoute(route)
     .map(([p1, p2]) => ({
       type: 'Feature',
@@ -316,7 +380,7 @@ const getRoute = (angle, step, polygon) => {
   return sortRoute(route)
 }
 
-const frameFeature = (CoverturaW, CoverturaH, angle, origin, id) => {
+const frame = (CoverturaW, CoverturaH, angle, origin) => {
   const hypot = Math.hypot(CoverturaH , CoverturaW) / 200000
   const offsetAngle = (angle - 90) % 360
   const teta = Math.grados(Math.atan2(CoverturaH , CoverturaW))
@@ -327,7 +391,6 @@ const frameFeature = (CoverturaW, CoverturaH, angle, origin, id) => {
   const x4 = turf.rhumbDestination(origin, hypot, 360 - teta + offsetAngle).geometry.coordinates
     
   return {
-    // id: `frame-${id}`,
     type: 'Feature',
     properties: {},
     geometry: { type: 'Polygon', coordinates: [[
@@ -340,22 +403,57 @@ const frameFeature = (CoverturaW, CoverturaH, angle, origin, id) => {
   }
 } 
 
+// Not is the best route only findNearest
 const findNearest = (point, lines) => {
   const { nearest, others } = lines
     .map(l => ({ p: l, d: l.map(p => turf.distance(point, p)) }))
     .reduce((result, current) => {
-      if (result.nearest === null) return {...result, nearest: current}
-      if (Math.min(...result.nearest.d) < Math.min(...current.d)) return {...result, others: [...result.others, current] }
+      
+      if (result.nearest === null) {
+        return {
+          ...result,
+          nearest: current
+        }
+      }
+      
+      if (Math.min(...result.nearest.d) < Math.min(...current.d)) {
+        return {
+          ...result, 
+          others: [...result.others, current] 
+        }
+      }
+
       return { nearest: current, others: [result.nearest, ...result.others] }
     }, { nearest: null, others: [] })
 
-  if (nearest.d[0] < nearest.d[1]) return [nearest.p, ...others.map(o => o.p)]
-  return [[nearest.p[1],nearest.p[0]], ...others.map(o => o.p)]
+
+  return [
+    nearest.d[0] < nearest.d[1] ? nearest.p : [nearest.p[1],nearest.p[0]], 
+    ...others.map(o => o.p)
+  ]
 }
 
-const sortRoute = lines => {
-  if (lines.length === 1) {
-    return lines
+const sortRoute = lines => lines.length === 1 
+  ? lines
+  : [lines[0], ...sortRoute(findNearest(lines[0][1], lines.slice(1)))]
+
+
+map.on('draw.create', e => {
+  const polygon = {
+    ...e.features[0],
+    id: 'polygon',
   }
-  return [lines[0], ...sortRoute(findNearest(lines[0][1], lines.slice(1)))]
-}
+  draw.getAll().features.map(f => draw.delete(f.id))
+  draw.add(polygon)
+  updateRoute(draw, polygon, control)
+})
+
+map.on('draw.update', () => {
+  draw.getAll().features.map(f => f.id !== 'polygon' && draw.delete(f.id))
+  updateRoute(draw, draw.get('polygon'), control)
+})
+
+emitter.on('control.update', () => {
+  draw.getAll().features.map(f => f.id !== 'polygon' && draw.delete(f.id))
+  updateRoute(draw, draw.get('polygon'), control)
+});
