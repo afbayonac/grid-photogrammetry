63b2528dba6c6abb4727050ecbbdde50f173206a
aratoca <afbayonac@gmail.com>
2022-11-26 19:52:53 -0500

    Add cameras

--- droneModels.js
+++ droneModels.js
@@ -6,5 +6,222 @@ export default [
     imageHeight: 3648,
     sensorWidth: 13.31,
     sensorHeight: 8.88
+  }, {
+    name: 'Nikon aw100',
+    focalLength: 5,
+    imageHeight: 3456,
+    imageWidth: 4608,
+    sensorHeight: 4.62,
+    sensorWidth: 6.16
+  }, {
+    name: 'Canon SX230 HS',
+    focalLength: 5,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 4.62,
+    sensorWidth: 6.16
+  }, {
+    name: ' Samsung s860',
+    focalLength: 6.3,
+    imageHeight: 2448,
+    imageWidth: 3264,
+    sensorHeight: 4.29,
+    sensorWidth: 5.76
+  }, {
+    name: 'Canon SX260-SX280',
+    focalLength: 4.5,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'Nex5 25mm',
+    focalLength: 25,
+    imageHeight: 3056,
+    imageWidth: 4592,
+    sensorHeight: 15.6,
+    sensorWidth: 23.4
+  }, {
+    name: 'Nex5 16mm',
+    focalLength: 16,
+    imageHeight: 3056,
+    imageWidth: 4592,
+    sensorHeight: 15.6,
+    sensorWidth: 23.4
+  }, {
+    name: 'Nex7 16mm',
+    focalLength: 16,
+    imageHeight: 4000,
+    imageWidth: 6000,
+    sensorHeight: 15.6,
+    sensorWidth: 23.5
+  }, {
+    name: 'Canon S110-S120',
+    focalLength: 5.2,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 5.7,
+    sensorWidth: 7.6
+  }, {
+    name: 'Canon ELPH 520HS',
+    focalLength: 4,
+    imageHeight: 2736,
+    imageWidth: 3648,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'Canon ELPH 330HS',
+    focalLength: 4.3,
+    imageHeight: 2736,
+    imageWidth: 3648,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'Canon ELPH 115-130 IS',
+    focalLength: 5,
+    imageHeight: 3072,
+    imageWidth: 4608,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'Canon A2300-A2400',
+    focalLength: 5,
+    imageHeight: 3456,
+    imageWidth: 4608,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'Canon 5D Mk2',
+    focalLength: 5,
+    imageHeight: 3744,
+    imageWidth: 5616,
+    sensorHeight: 24,
+    sensorWidth: 36
+  }, {
+    name: 'NX1000',
+    focalLength: 20,
+    imageHeight: 3648,
+    imageWidth: 5472,
+    sensorHeight: 15.7,
+    sensorWidth: 23.5
+  }, {
+    name: 'FIREFLY 6S',
+    focalLength: 3,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 4.7058,
+    sensorWidth: 6.2372
+  }, {
+    name: 'Sony A6000',
+    focalLength: 20,
+    imageHeight: 4000,
+    imageWidth: 6000,
+    sensorHeight: 15.6,
+    sensorWidth: 23.5
+  }, {
+    name: 'Sony A7R',
+    focalLength: 28,
+    imageHeight: 4912,
+    imageWidth: 7360,
+    sensorHeight: 24.0,
+    sensorWidth: 35.9
+  }, {
+    name: 'Parrot Sequoia Monochrome',
+    focalLength: 3.98,
+    imageHeight: 960,
+    imageWidth: 1280,
+    sensorHeight: 3.6,
+    sensorWidth: 4.8
+  }, {
+    name: 'Parrot Sequoia RGB',
+    focalLength: 4.88,
+    imageHeight: 3456,
+    imageWidth: 4608,
+    sensorHeight: 4.63,
+    sensorWidth: 6.17
+  }, {
+    name: 'Canon S120',
+    focalLength: 5.2,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 5.7,
+    sensorWidth: 7.6
+  }, {
+    name: 'WX 500',
+    focalLength: 4.1,
+    imageHeight: 3264,
+    imageWidth: 4896,
+    sensorHeight: 4.55,
+    sensorWidth: 6.17
+  }, {
+    name: 'RedEdge',
+    focalLength: 5.5,
+    imageHeight: 960,
+    imageWidth: 1280,
+    sensorHeight: 3.6,
+    sensorWidth: 4.8
+  }, {
+    name: 'FLIR Vue 336 6,8mm',
+    focalLength: 6.8,
+    imageHeight: 256,
+    imageWidth: 336,
+    sensorHeight: 4.352,
+    sensorWidth: 5.712
+  }, {
+    name: 'FLIR Vue 336 9mm',
+    focalLength: 9,
+    imageHeight: 256,
+    imageWidth: 336,
+    sensorHeight: 4.352,
+    sensorWidth: 5.712
+  }, {
+    name: 'FLIR Vue 336 13mm',
+    focalLength: 13,
+    imageHeight: 256,
+    imageWidth: 336,
+    sensorHeight: 4.352,
+    sensorWidth: 5.712
+  }, {
+    name: 'FLIR Vue 640 9mm',
+    focalLength: 9,
+    imageHeight: 512,
+    imageWidth: 640,
+    sensorHeight: 8.704,
+    sensorWidth: 10.88
+  }, {
+    name: 'FLIR Vue 640 13mm',
+    focalLength: 13,
+    imageHeight: 512,
+    imageWidth: 640,
+    sensorHeight: 8.704,
+    sensorWidth: 10.88
+  }, {
+    name: 'FLIR Vue 640 19mm',
+    focalLength: 19,
+    imageHeight: 512,
+    imageWidth: 640,
+    sensorHeight: 8.704,
+    sensorWidth: 10.88
+  }, {
+    name: 'GoPro Hero 4 Black Wide',
+    focalLength: 2.46,
+    imageHeight: 3000,
+    imageWidth: 4000,
+    sensorHeight: 4.56,
+    sensorWidth: 6.17
+  }, {
+    name: 'Sony a7R2',
+    focalLength: 5,
+    imageHeight: 3456,
+    imageWidth: 4608,
+    sensorHeight: 4.62,
+    sensorWidth: 6.16
+  }, {
+    name: 'AeroHawk_8MP',
+    focalLength: 3.039,
+    imageHeight: 2464,
+    imageWidth: 3280,
+    sensorHeight: 2.76,
+    sensorWidth: 3.674
   }
 ]
